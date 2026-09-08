13eb424f3ec4a7740a6caa7c16b078b032240aab
ADMIN <josh.dave.bengaura@gmail.com>
2026-09-08 18:36:52 +0300

    completed react-start

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,9 +1,13 @@
+import Home from './components/Home.jsx';
 import './App.css';
+
 const App = () => {
   return (
     <>
       <h1>My App</h1>
+      <Home />
     </>
   );
 };
+
 export default App;
