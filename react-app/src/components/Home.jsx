f2eccf6b676bd51251322848ffb1a29251d275bd
ADMIN <josh.dave.bengaura@gmail.com>
2026-09-09 12:47:47 +0300

    initially started state-management

--- react-app/src/components/Home.jsx
+++ react-app/src/components/Home.jsx
@@ -1,40 +1,42 @@
-import MediaRow from './MediaRow';
+import MediaRow from "./MediaRow";
+import { useState } from "react";
 
 const Home = () => {
+  const [selectedItem, setSelectedItem] = useState(null);
   const mediaArray = [
     {
       media_id: 8,
       user_id: 5,
-      filename: 'https://placecats.com/1200/800',
-      thumbnail: 'https://placecats.com/320/240',
+      filename: "https://placecats.com/1200/800",
+      thumbnail: "https://placecats.com/320/240",
       filesize: 170469,
-      media_type: 'image/jpeg',
-      title: 'Picture 1',
-      description: 'This is a placeholder picture.',
-      created_at: '2024-01-07T20:49:34.000Z',
+      media_type: "image/jpeg",
+      title: "Picture 1",
+      description: "This is a placeholder picture.",
+      created_at: "2024-01-07T20:49:34.000Z",
     },
     {
       media_id: 9,
       user_id: 7,
-      filename: 'https://placecats.com/800/600',
-      thumbnail: 'https://placecats.com/240/240',
+      filename: "https://placecats.com/800/600",
+      thumbnail: "https://placecats.com/240/240",
       filesize: 1002912,
-      media_type: 'image/jpeg',
-      title: 'Pic 2',
-      description: '',
-      created_at: '2024-01-07T21:32:27.000Z',
+      media_type: "image/jpeg",
+      title: "Pic 2",
+      description: "",
+      created_at: "2024-01-07T21:32:27.000Z",
     },
     {
       media_id: 17,
       user_id: 2,
       filename:
-        'https://upload.wikimedia.org/wikipedia/commons/transcoded/0/0f/Cat_playing_with_its_claws.webm/Cat_playing_with_its_claws.webm.240p.vp9.webm',
-      thumbnail: 'https://placecats.com/350/350',
+        "https://upload.wikimedia.org/wikipedia/commons/transcoded/0/0f/Cat_playing_with_its_claws.webm/Cat_playing_with_its_claws.webm.240p.vp9.webm",
+      thumbnail: "https://placecats.com/350/350",
       filesize: 1236616,
-      media_type: 'video/mp4',
-      title: 'Bunny',
-      description: 'Butterflies fly around the bunny.',
-      created_at: '2024-01-07T20:48:13.000Z',
+      media_type: "video/mp4",
+      title: "Bunny",
+      description: "Butterflies fly around the bunny.",
+      created_at: "2024-01-07T20:48:13.000Z",
     },
   ];
   return (
--- react-app/src/components/MediaRow.jsx
+++ react-app/src/components/MediaRow.jsx
@@ -1,7 +1,7 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const MediaRow = (props) => {
-  const {item} = props;
+  const { item } = props;
   return (
     <tr key={item.media_id}>
       <td>
@@ -9,7 +9,7 @@ const MediaRow = (props) => {
       </td>
       <td>{item.title}</td>
       <td>{item.description}</td>
-      <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
+      <td>{new Date(item.created_at).toLocaleString("fi-FI")}</td>
       <td>{item.filesize}</td>
       <td>{item.media_type}</td>
     </tr>
